6436eb8e0dab31f7985a8e21a5e2607d75c97cac
mehediCikatech <mehedi.cikatech@gmail.com>
2023-02-13 22:10:55 +0600

    Revert "assignment 5 done"
    This reverts commit 5fed22aecc45a57bf6bced00358c1407583aabd8.

--- assignment-5/Middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err,req,res,next) => {
-    const status = res.statusCode? res.statusCode : 500;
-    res.status(status);
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV == 'development'? err.stack : ''
-    });
-}
-module.exports = errorHandler;
--- assignment-5/Routes/Auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController  = require('../Controllers/AuthController');
-router.post('/register', AuthController.Register);
-router.post('/login', AuthController.Login);
-router.post('/logout', AuthController.Logout);
-module.exports = router;
